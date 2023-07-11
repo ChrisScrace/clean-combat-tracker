ffc0e31d51efed691e69ce8833632b185855dff5
Chris Scrace <chrisscrace@gmail.com>
2023-07-11 18:41:55 +0100

    removing NPC from combat track on death

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+# 1.0.0
+* When an NPC reaches zero HP they are removed from the combat tracker.
--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,23 @@
-Hooks.once('init', async function() {
+const ID = "clean-combat-tracker"
 
+Hooks.on('updateActor', async function(actor, change) {
+    if (game.user.role == 4) {
+        if(actor.type === "npc" && change.system.attributes.hp.value === 0) {
+            let combatant = game.combat.getCombatantByActor(actor._id);
+            if(combatant){
+                combatant.delete();
+                log(false, combatant);
+            }
+        }
+    }
 });
 
-Hooks.once('ready', async function() {
+Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => { registerPackageDebugFlag(ID); });
 
-});
+function log(force, ...args) {
+    const shouldLog = force || game.modules.get('_dev-mode')?.api?.getPackageDebugValue(ID);
+
+    if (shouldLog) {
+        console.log(ID, '|', ...args);
+    }
+}
